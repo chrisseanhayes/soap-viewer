b50ffa9d58c0cbadd458025e7e52c6ab575b2be7
Chris Hayes <chrishayescodes@gmail.com>
2026-08-26 21:13:12 -0700

    remove original static site

--- soap/scratch/test.js
+++
@@ -1,3 +0,0 @@
-const fs = require('fs');
-const js = fs.readFileSync('/Users/chrishayes/Documents/projects/chrishayescodes/soap/js/mermaid-init.js', 'utf8');
-console.log("No syntax errors. Can we find a runtime error?");
